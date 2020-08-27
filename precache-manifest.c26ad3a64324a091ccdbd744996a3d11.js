e578e5b4466a974383f002d8ce32b79d13f92768
beqa <badagadzeb@gmail.com>
2020-08-27 14:18:14 +0400

    Updates

---
+++ precache-manifest.c26ad3a64324a091ccdbd744996a3d11.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49bad057f528dcba7e57223e74a6e37",
+    "url": "/arena/index.html"
+  },
+  {
+    "revision": "4e74652d364a900ec7ba",
+    "url": "/arena/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "9262bab0a1fe22a0f2af",
+    "url": "/arena/static/css/main.1a60e9a2.chunk.css"
+  },
+  {
+    "revision": "4e74652d364a900ec7ba",
+    "url": "/arena/static/js/2.5bf0399d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/arena/static/js/2.5bf0399d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9262bab0a1fe22a0f2af",
+    "url": "/arena/static/js/main.f31a9bf3.chunk.js"
+  },
+  {
+    "revision": "8c7cf8d19eaba1a0457e",
+    "url": "/arena/static/js/runtime-main.72b59427.js"
+  },
+  {
+    "revision": "eb53abfa282b0e15b09310be2dcfcd75",
+    "url": "/arena/static/media/1.eb53abfa.svg"
+  },
+  {
+    "revision": "e10d246776ed73e84c607fa8d7126dc3",
+    "url": "/arena/static/media/2.e10d2467.svg"
+  },
+  {
+    "revision": "810b83cfec5078fee8ae9cf064c5313e",
+    "url": "/arena/static/media/3.810b83cf.svg"
+  },
+  {
+    "revision": "57c8e3f49afb0cb26ebbeaa2776156a6",
+    "url": "/arena/static/media/Facebook.57c8e3f4.svg"
+  },
+  {
+    "revision": "044e48e55de1d3aa472f119613618d26",
+    "url": "/arena/static/media/Instagram.044e48e5.svg"
+  },
+  {
+    "revision": "5a8695ae0d33473e961d2064e53183fb",
+    "url": "/arena/static/media/Knight1-1.5a8695ae.svg"
+  },
+  {
+    "revision": "9fbc39f3dfb3ae97b896c9b4bd79fb24",
+    "url": "/arena/static/media/Knight2-2.9fbc39f3.svg"
+  },
+  {
+    "revision": "2dd7dca82be4ec2a32d938c2b92ceea4",
+    "url": "/arena/static/media/Knight3-3.2dd7dca8.png"
+  },
+  {
+    "revision": "dc35bba9051c4b316247c71f82eba5f2",
+    "url": "/arena/static/media/Knight4.dc35bba9.svg"
+  },
+  {
+    "revision": "bd5c947d74e43ae1fdd9592053caf407",
+    "url": "/arena/static/media/MYRIADPRO-REGULAR.bd5c947d.ttf"
+  },
+  {
+    "revision": "c980669629cce3518f9aebf8573cdbe7",
+    "url": "/arena/static/media/MYRIADPRO-REGULAR.c9806696.eot"
+  },
+  {
+    "revision": "d75d7f70aa160104a45a743eae7a8a7e",
+    "url": "/arena/static/media/MYRIADPRO-REGULAR.d75d7f70.svg"
+  },
+  {
+    "revision": "e3e21ec37205a621b95d56a801bc0856",
+    "url": "/arena/static/media/Twitter.e3e21ec3.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/arena/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fa0ad945364455c0c568a430d87edeb0",
+    "url": "/arena/static/media/yelloCircle.fa0ad945.svg"
+  }
+]);
