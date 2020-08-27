440c448be947cb4c6f3026b2ee2cf3a92627ab55
beqa <badagadzeb@gmail.com>
2020-08-27 14:58:46 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arena/precache-manifest.c26ad3a64324a091ccdbd744996a3d11.js"
+  "/arena/precache-manifest.af06c2aa702dc9816327ec1ba2099534.js"
 );
 
 self.addEventListener('message', (event) => {
